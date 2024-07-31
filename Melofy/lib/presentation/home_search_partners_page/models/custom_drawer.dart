54d681ac3199392a4251e3445ba0ea1256ac2cdf
Hayden9898 <haydenc9898@gmail.com>
2024-07-31 15:06:18 -0400

    Added topBar for drawer screen and re-edited the display format

--- Melofy/lib/presentation/home_search_partners_page/models/custom_drawer.dart
+++ Melofy/lib/presentation/home_search_partners_page/models/custom_drawer.dart
@@ -2,6 +2,7 @@ import 'dart:ffi';
 import 'package:Melofy/API/api_calls.dart';
 import 'package:Melofy/API/models/artist.dart';
 import 'package:Melofy/API/models/track.dart';
+import 'package:Melofy/core/app_export.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,49 +12,75 @@ class CustomDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-  return Drawer(
-    shape:
-    const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
+    return Drawer(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(30),
+            topLeft: Radius.circular(0),
+            bottomLeft: Radius.circular(0),
+            bottomRight: Radius.circular(30)),
+      ),
+      child: FutureBuilder(
+        future: MakeAPICall.getArtistandTracks(artistID!),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            Artist artist = snapshot.data![0];
+            List<Track> tracks = snapshot.data![1];
+            int? followers = artist.followers!.total;
+            //Array artistPhoto = artist.images
 
-                  topRight: Radius.circular(0),
-                  topLeft: Radius.circular(0),
-                  bottomLeft: Radius.circular(0),
-                  bottomRight: Radius.circular(0)),
-                  
-    ),
-    child: FutureBuilder(
-      future: MakeAPICall.getArtistandTracks(artistID!),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          
-          Artist artist = snapshot.data![0];
-          List<Track> tracks = snapshot.data![1];        
-          int? followers = artist.followers!.total;
-         //Array artistPhoto = artist.images
-
-          return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  artist.name! + "\n" + tracks.first.name!,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 20),
-                Text(
-                  'Followers: $followers',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                
-              ],
-            ),
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-      },
-    ),
-  );
-}
+            return Container(
+              child: Column(
+                children: [
+                  Row(
+                    //mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(16),
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Icon(Icons.arrow_back),
+                        ),
+                      ),
+                      Align(
+                        
+                        alignment: Alignment.center,
+                        child: Text("About This Artist", textAlign: TextAlign.center),
+                      )
+                    ],
+                  ), 
+                  SizedBox( 
+                    child: Column(
+                    children:[
+                  Text(
+                    artist.name!,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black
+                    ),
+                  ),
+                  Text(
+                    '$followers Followers',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.grey
+                      ),
+                   ),
+                    ],
+                    ),
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
 }
