87ba6ce94e167885996a89fb230870bb883aac89
Zanan Virani <viranizanan@gmail.com>
2024-12-15 16:58:41 -0500

    Commented main

--- Melofy/lib/main.dart
+++ Melofy/lib/main.dart
@@ -1,39 +1,34 @@
+/**
+ * Entry point of the application, where the application starts running the program.
+ */
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'core/app_export.dart';
 import 'data/pref_data/pref_data.dart';
 
+/**
+ * Main function of the application, where the application starts running the program.
+ */
 Future<void> main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-
-  // if (kIsWeb) {
-  //   await Firebase.initializeApp(
-  //       options: FirebaseOptions(
-  //           apiKey: "AIzaSyB66EQNOWWouIZ4oEP978q0v_9pMTIySlo",
-  //           authDomain: "mana-music-9aebf.firebaseapp.com",
-  //           projectId: "mana-music-9aebf",
-  //           storageBucket: "mana-music-9aebf.appspot.com",
-  //           messagingSenderId: "588489666068",
-  //           appId: "1:588489666068:web:e626cd8daf29286d41c101",
-  //           measurementId: "G-388NLBEJCF"));
-  // } else {
-  //   await Firebase.initializeApp();
-  // }
   WidgetsFlutterBinding.ensureInitialized();
+  // Set the preferred orientation of the application to portraitUp
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]).then((value) {
     Logger.init(kReleaseMode ? LogMode.live : LogMode.debug);
   });
+  // Initialize the shared preferences for persisting data storage.
   await PrefData().init();
-  //await PrefData.clearPreferencesData();
+  // Run the application
   runApp(MyApp());
 }
 
+/**
+ * MyApp class is the root widget of the application, where the application starts running the program.
+ */
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return PopScope(
@@ -49,7 +44,8 @@ class MyApp extends StatelessWidget {
             fallbackLocale: Locale('en', 'US'),
             title: 'Melofy',
             initialBinding: InitialBindings(),
-            initialRoute: AppRoutes.initialRoute,
+            initialRoute:
+                AppRoutes.initialRoute, //initial route of  the application
             getPages: AppRoutes.pages,
           ),
         );
